c4891120ca66b91597835825ee44a027222d7450
Felipe Intelangelo <felipeintelangelo@gmail.com>
2025-10-20 11:55:51 -0300

    Project Creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Register } from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "auth/register", component: Register}
+];
