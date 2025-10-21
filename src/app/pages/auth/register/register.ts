8349ca188880367f16c09ec0f9905e7098c9cd64
Julian Barreiro <barreirojulian2k@gmail.com>
2025-10-21 20:53:41 -0300

    filled the fieldsets
    filled the fieldsets that were missing for the api

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -18,12 +18,23 @@ export class Register implements OnInit{
     this.registerForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      nickname: new FormControl('', Validators.required),
     });
   }
 
+  get username() { return this.registerForm.get('username'); }
+  get password() { return this.registerForm.get('password'); }
+  get email() { return this.registerForm.get('email'); }
+  get name() { return this.registerForm.get('name'); }
+  get lastName() { return this.registerForm.get('lastName'); }
+  get nickname() { return this.registerForm.get('nickname'); }
+
   onSubmit(){
-    console.log(this.registerForm.value)
+    console.log(this.username?.value)
+    alert('Registrado correctamente')
     this.router.navigate(['auth/login']);
   }
 }
