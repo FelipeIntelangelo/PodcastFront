a28e9ba586d1f3247a2d7ff2d0f0ac48b0027d4a
Julian Barreiro <barreirojulian2k@gmail.com>
2025-10-21 19:22:49 -0300

    minor changes

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit{
+  registerForm!: FormGroup;
 
+  constructor(public router:Router){}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.registerForm.value)
+    this.router.navigate(['auth/login']);
+  }
 }
